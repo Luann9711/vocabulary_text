b79a80fcd99e97282b63505870fb5a50ea5595b4
luann9711 <marchen9708@gmail.com>
2021-12-12 19:56:25 +0900

    완성

--- vocabulary.py
+++ vocabulary.py
@@ -1,8 +1,25 @@
 
+while True:
+    eng = ''
+    kor = ''
+    q = "q"
 
-eng = input('영어 단어를 입력하세요: ')
-kor = input('한국어 뜻을 입력하세요: ')
+    eng = input('영어 단어를 입력하세요: ')
+    if eng == q:
+        break
+    
+    else:
+        pass
 
-with open('vocabulary.txt', 'a') as v:
-    v.write(eng)
-    v.write(kor)
+    kor = input('한국어 뜻을 입력하세요: ')
+ 
+    if kor == q:
+        break
+
+    else:
+        pass
+
+
+    with open('practice/mini_project/vocabulary/vocabulary_text/vocabulary.txt', 'a') as v:
+        v.write(eng+': ')
+        v.write(kor+'\n')
