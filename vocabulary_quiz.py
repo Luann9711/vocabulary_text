4b40ab50e1bf6490d4c02faf3e3ad09c1135a2f6
luann9711 <marchen9708@gmail.com>
2021-12-13 21:25:59 +0900

    randomized
    make the vocbulary quiz  ask the random question in the txt file

--- vocabulary_quiz.py
+++ vocabulary_quiz.py
@@ -1,13 +1,26 @@
+import random
+
+voca = {}
+q = "q"
+
 with open('practice/mini_project/vocabulary/vocabulary_text/vocabulary.txt', 'r') as v:
 
-    for i in v:
-        line = (i.strip().split(': '))
-        eng = line[0]
-        kor = line[1]
+    for line in v:
+        data = line.strip().split(': ')
+        eng, kor = data[0], data[1]
 
-        answer_voca = input('{}: '.format(eng))
+        voca[eng] = kor
 
-        if answer_voca == kor:
-            print('맞았습니다!')
-        elif answer_voca != kor:
-            print('아쉽습니다. 정답은 {}입니다.'.format(kor))
+
+    while True:
+        mix_voca = random.choice(list(voca.items()))
+        random_eng, random_kor = mix_voca[0], mix_voca[1]
+
+        answer_voca = input('{}: '.format(random_eng))
+        
+        if answer_voca == q:
+            break
+        elif answer_voca == random_kor:
+            print('맞았습니다!\n')
+        elif answer_voca != random_kor:
+            print('아쉽습니다. 정답은 {}입니다.\n'.format(random_kor))
